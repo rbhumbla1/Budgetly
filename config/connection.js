a8931dc4239509809a6261849e25bc60d293373f
Rima Bhumbla <rima.bhumbla@gmail.com>
2023-02-10 09:53:55 +0530

    use DB_HOST & DB_PORT
    use DB_HOST & DB_PORT

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      //heroku
+      //heroku - use DB_HOST & DB_PORT
       host: process.env.DB_HOST,
       dialect: 'mysql',
       port: process.env.DB_PORT
