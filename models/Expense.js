fc977ddbfc33995b71d80c8e012a9526741072de
rbhum <rima.bhumbla@gmail.com>
2023-02-15 11:11:01 +0530

    changed Budget & Expense models and seed data

--- models/Expense.js
+++ models/Expense.js
@@ -11,20 +11,11 @@ Expense.init(
             primaryKey: true,
             autoIncrement: true,
         },
-        savings: {
+        amount_spent: {
             type: DataTypes.FLOAT,
         },
-        house_loan: {
-            type: DataTypes.FLOAT,
-        },
-        food: {
-            type: DataTypes.FLOAT,
-        },
-        transportation: {
-            type: DataTypes.FLOAT,
-        },
-        personal: {
-            type: DataTypes.FLOAT,
+        note: {
+            type: DataTypes.STRING(100),
         },
         date_created: {
             type: DataTypes.DATE,
@@ -35,6 +26,13 @@ Expense.init(
             type: DataTypes.INTEGER,
             references: {
                 model: 'user',
+                key: 'id',
+            },
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'budget_category',
                 key: 'id',
             },
         },
