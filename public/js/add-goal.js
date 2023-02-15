a13fc9ee3a353ad053bab16bd79bcf1aac9dd842
Deorren Erive <erive92d@gmail.com>
2023-02-14 19:20:07 -0800

    changed delete button

--- public/js/add-goal.js
+++ public/js/add-goal.js
@@ -77,3 +77,28 @@ const newFormHandler = async (e) => {
 
 formNewGoal.addEventListener('submit', newFormHandler);
 
+
+const delBtn = document.querySelectorAll('.del-btn')
+
+for(let i = 0; i < delBtn.length; i++) {
+  delBtn[i].addEventListener("click", async (e)=>{
+    let currentCategory = e.target.getAttribute('data-id')
+
+    console.log(currentCategory)
+    const response = await fetch(`/api/budgets/${currentCategory}`, {
+      method: 'DELETE',
+    });
+  
+    if (response.ok) {
+      document.location.replace('/api/budgets/goals');
+    } else {
+      alert('Failed to delete the goal.');
+    }
+    
+  
+  })
+
+}
+
+
+
--- public/js/pie-chart.js
+++ public/js/pie-chart.js
@@ -1,54 +1,59 @@
+// // Step 3
+// // var svg = d3.select("svg");
+// let displayPie = document.querySelector(".chart-inputs");
+// // let width = svg.getAttribute("width");
+// // let height = svg.getAttribute("height");
+// // let radius = 200;
+// // var chartData = [];
 
-        // Step 3
-        var svg = d3.select("svg"),
-            width = svg.attr("width"),
-            height = svg.attr("height"),
-            radius = 200;
+fetch('/api/budgets', {
+  method: 'GET',
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data)
+     var svg = d3.select("svg"),
+     width = svg.attr("width"),
+     height = svg.attr("height"),
+     radius = 150;
 
-        // Step 1        
-        var data = [{name: "Transportation", share:50.00}, 
-                    {name: "Rent", share: 100.00},
-                    {name: "Example", share: 15.42},
-                    {name: "Example", share: 13.65},
-                    {name: "Remaining", share: 150.00},
-                ];
-        
-        var g = svg.append("g")
-                   .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+ var g = svg.append("g")
+            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-        // Step 4
-        var ordScale = d3.scaleOrdinal()
-                        	.domain(data)
-                        	.range(['#ffd384','#94ebcd','#fbaccc','#d3e0ea','#fa7f72']);
+ // Step 4
+ var ordScale = d3.scaleOrdinal()
+                     .domain(data.budgets)
+                     .range(['#ffd384','#94ebcd','#fbaccc','#d3e0ea','#fa7f72']);
 
-        // Step 5
-        var pie = d3.pie().value(function(d) { 
-                return d.share; 
-            });
+ // Step 5
+ var pie = d3.pie().value(function(d) { 
+         return d.amount; 
+     });
 
-        var arc = g.selectAll("arc")
-                   .data(pie(data))
-                   .enter();
+ var arc = g.selectAll("arc")
+            .data(pie(data.budgets))
+            .enter();
 
-        // Step 6
-        var path = d3.arc()
-                     .outerRadius(radius)
-                     .innerRadius(0);
+ // Step 6
+ var path = d3.arc()
+              .outerRadius(radius)
+              .innerRadius(0);
 
-        arc.append("path")
-           .attr("d", path)
-           .attr("fill", function(d) { return ordScale(d.data.name); });
+ arc.append("path")
+    .attr("d", path)
+    .attr("fill", function(d) { return ordScale(d.data.amount); });
 
-        // Step 7
-        var label = d3.arc()
-                      .outerRadius(radius)
-                      .innerRadius(0);
-            
-        arc.append("text")
-           .attr("transform", function(d) { 
-                    return "translate(" + label.centroid(d) + ")"; 
-            })
-           .text(function(d) { return d.data.name; })
-           .style("font-family", "arial")
-           .style("font-size", 15);
-            
+ // Step 7
+ var label = d3.arc()
+               .outerRadius(radius)
+               .innerRadius(0);
+     
+ arc.append("text")
+    .attr("transform", function(d) { 
+             return "translate(" + label.centroid(d) + ")"; 
+     })
+    .text(function(d) { return d.data.category_id; })
+    .style("font-family", "arial")
+    .style("font-size", 15);
+     
+  });
